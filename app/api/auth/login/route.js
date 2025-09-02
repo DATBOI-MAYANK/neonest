e072bdd8346aabe073eeae859ee740d045fc8040
Mayank Roy <mayankroy106106@gmail.com>
2025-09-02 08:55:24 +0530

    Add Argon2

--- app/api/auth/login/route.js
+++ app/api/auth/login/route.js
@@ -1,7 +1,7 @@
 import User from "@/app/models/User.model";
 import connectDB from "@/lib/connectDB";
 import bcryptjs from "bcryptjs";
-import jwt from 'jsonwebtoken'
+import jwt from "jsonwebtoken";
 
 export async function POST(req) {
   await connectDB();
@@ -10,26 +10,36 @@ export async function POST(req) {
     const { email, password } = body;
 
     if (!email || !password) {
-      return Response.json(
-        { error: "Please provide all details" },
-        { status: 422 }
-      );
+      return Response.json({ error: "Please provide all details" }, { status: 422 });
     }
 
     const userExists = await User.findOne({ email: email.toLowerCase() });
     if (!userExists) {
-      return Response.json(
-        { error: "no such user exists! signup instead" },
-        { status: 422 }
-      );
+      return Response.json({ error: "no such user exists! signup instead" }, { status: 422 });
     }
 
-    const hashPass = await bcryptjs.compare(password , userExists.password);
-    if(!hashPass){
-      return Response.json(
-        { error: "wrong password" },
-        { status: 400 }
-      );
+    // const hashPass = await bcryptjs.compare(password , userExists.password);
+    // if(!hashPass){
+    //   return Response.json(
+    //     { error: "wrong password" },
+    //     { status: 400 }
+    //   );
+    // }
+    if (userExists.password.startsWith("$2")) {
+      const match = await bcrypt.compare(password, userExists.password);
+      if (!match) {
+        return Response.json({ error: "wrong password" }, { status: 400 });
+      }
+      userExists.password = await argon2.hash(password, { type: argon2.argon2id });
+
+      await userExists.save();
+    } else {
+      const match = await argon2.verify(userExists.password, password, {
+        type: argon2.argon2id,
+      });
+      if (!match) {
+        return Response.json({ error: "wrong password" }, { status: 400 });
+      }
     }
 
     const token = jwt.sign(
